4242c8a4bcdbc398276ebf092b2eceea2a8d188a
Ian <typppo@gmail.com>
2013-01-20 21:34:25 -0800

    cleaning and adding more content

--- conspiracy.py
+++ conspiracy.py
@@ -5,53 +5,56 @@ import re
 
 VARS = {
 
-'{{real_noun}}': 'oil, guns, Ebola Virus, flourine, alternative medicine, chemtrails, flourine, GMOs, DNA, manchurian candidates, election polls, nuclear power, nuclear weapons, smallpox',
+'{{good_noun}}': 'the bible, money, donations,',
 
-'{{concept}}': 'sex, the bible, money, the government, the media, unemployment, Islam, Judaism, the stock market, time, death, old age, cancer, diversity, communism, socialism, AIDS',
+'{{malady}}': 'cancer, back pain, bipolar disorder, depression, decreased intelligence, foot fungus, mad cow disease, diabetes, autism, ulcers, allergies, Celiac\'s disease, Alzheimer\'s, Parkinson\'s, heart disease, restless leg syndrome, schizophrenia, ADHD, high blood pressure, chronic fatigue syndrome, Black Lung disease, myopia, age spots, melanoma, breast cancer, varicose veins, balding, anatidaephobia, paranoia, hyperpigmentation of the skin, albinism, cataracts, cavities, dwarfism, ',
 
-'{{government_noun}}': 'the FBI, the CIA, the USA, the UK, the Taliban, NASA, Russia, the FDA, NATO, FEMA, the KGB',
+'{{dangerous_noun}}': 'oil, guns, Ebola Virus, flourine, alternative medicine, chemtrails, flourine, GMOs, pesticides, nuclear power, nuclear weapons, smallpox, cancer, sucralose, DDT, trace heavy metals, lead, radioactive isotopes, arsenic,vaccines',
 
-'{{company}}': 'Google, Apple, Exxon, Haliburton, BP, Texaco, Lehman Brothers, Facebook',
+'{{era}}': 'biblical times, the American Revolution, the Vietnam War, WWI, WWII, the Civil War era,',
+
+'{{abstract_noun}}': 'sex, money, the government, the media, unemployment, Islam, Judaism, the stock market, old age, diversity, communism, socialism, AIDS, manchurian candidates, election polls, the bible, peace, welfare, free food, freedom from oppression, tax rebates, the Republican playbook, the Democratic playbook, the Libertarian playbook, the gay agenda, gay marriage, "equality", the economy, feminism, ',
+
+'{{government_org}}': 'the FBI, the CIA, the USA, the UK, the Taliban, NASA, Russia, the FDA, NATO, FEMA, the KGB,',
+
+'{{company}}': 'Google, Apple, Exxon, Halliburton, BP, Texaco, Lehman Brothers, Facebook',
 
 '{{country}}': 'the USA, the UK, Russia, Iran, Iraq, Afghanistan',
 
-'{{organization}}': 'Republicans, Democrats, Communists, Socialists, KKK, Libertarians, Occupy Wall Street, Black Panthers, The Tea Party, Big Oil, Big Pharma, Big Data, The Knights Templar, Freemasons, Illuminati, Opus Dei, Skull and Bones, Shadow Government, the Mafia, the Mob, Jews, Catholics, Atheists, Reptilians, the media, Islamic Fundamentalists, Christian Fundamentalists, minorities,',
+'{{organization}}': 'Republicans, Democrats, Communists, Socialists, KKK, Libertarians, Occupy Wall Street, Black Panthers, the Tea Party, Big Oil, Big Pharma, Big Data, the Knights Templar, Freemasons, Illuminati, Opus Dei, Skull and Bones, Shadow Government, the Mafia, the Mob, Jews, Catholics, a group of Atheists, Reptilians, the media, Islamic Fundamentalists, Christian Fundamentalists, minorities, Wikileaks, the Founding Fathers, Fox News, Scientology, Anonymous, Monsanto,',
 
-'{{event}}': 'the moon landing, the Holocaust, the JFK assassination, WW2, WW1, the Vietnam War, the MLK assassination, the Manhattan Project, Occupy Wall Street, the Bolshevik revolution, the 2008 financial crash, the US Election of 2000, Fukushima, the Deepwater Horizon spill, the war in Iraq, the Black Plague,',
+'{{event}}': 'the moon landing, the Holocaust, the JFK assassination, WW2, WW1, the Vietnam War, the MLK assassination, the Manhattan Project, Occupy Wall Street, the Bolshevik revolution, the 2008 financial crash, the US Election of 2000, Fukushima, the Deepwater Horizon spill, the war in Iraq, the Black Plague, the American Revolution,',
 
-'{{place}}': 'Area 51, the White House, the Moon, the Alaskan Wilderness, Israel, North Korea, Russia, Roswell, Chernobyl, Fukishima, Three Mile Island, San Andreas Fault, East Germany, Northern Ireland, ocean trenches, the Salt Caverns, Yucca Mountain, Iraq, Iran, Afghanistan, the International Space Station, Mars, AMES research center, Auschwitz',
+'{{place}}': 'Area 51, the White House, the Moon, the Alaskan Wilderness, Israel, North Korea, Russia, Roswell, Chernobyl, Fukishima, Three Mile Island, San Andreas Fault, East Germany, Northern Ireland, ocean trenches, the Salt Caverns, Yucca Mountain, Iraq, Iran, Afghanistan, the International Space Station, Mars, AMES research center, Auschwitz, Thomas Jefferson\'s home, the Vatican',
 
-'{{famous_person}}': 'Hugo Chavez, Barack Obama, Vladimir Putin, Osama Bin Laden, George W Bush, Bill Clinton, Madonna, JFK, J Edgar Hoover, Pink, The Beastie Boys, Kim Jong Un, Hitler, Abraham Lincoln, George Clooney, Lady Gaga, Marilyn Monroe, Dick Cheney, Karl Rove, Glenn Beck, Saddam Hussein, Mahmoud Ahmadinejad, Fidel Castro, Kim Jong Il, Kim Il Sung.',
-
-'{{searched_noun}}': sys.argv[1] if len(sys.argv) > 1 else 'searched_noun',
+'{{famous_person}}': 'Hugo Chavez, Barack Obama, Vladimir Putin, Osama Bin Laden, George W Bush, Bill Clinton, Madonna, JFK, J Edgar Hoover, Pink, The Beastie Boys, Kim Jong Un, Hitler, Abraham Lincoln, George Clooney, Lady Gaga, Marilyn Monroe, Dick Cheney, Karl Rove, Glenn Beck, Saddam Hussein, Mahmoud Ahmadinejad, Fidel Castro, Kim Jong Il, Kim Il Sung, Julian Assange, Al Gore, the Reverend Al Sharpton, the Reverend Jesse Jackson, Michelle Obama, Billy Graham, Bill O\'Reilly, Oprah, Tom Cruise, Jack Chick, Larry Page, Tom Cook,',
 
 }
 
-
-f = open('sentences_test', 'r')
-statement = '\n'.join(f.readlines())
+f = open('introductions', 'r')
+intro = '\n'.join(f.readlines())
 f.close()
 
 for x in VARS:
   VARS[x] = map(lambda x: x.strip(), VARS[x].split(','))
 
+def process(statement):
+  previously_used = {}
+  regex = re.compile('({{.*?}})')
+  ms = regex.findall(statement)
+  for m in ms:
+    if m.startswith('{{same_'):
+      prev_used_key = m.replace('same_', '')
+      word_choice = previously_used[prev_used_key]
+    else:
+      for i in range(0, 20):
+        word_choice = random.choice(VARS[m])
+        if m not in previously_used or word_choice != previously_used[m]:
+          break
 
-previously_used = {}
-regex = re.compile('({{.*?}})')
-ms = regex.findall(statement)
-for m in ms:
-  if m.startswith('{{same_'):
-    prev_used_key = m.replace('same_', '')
-    word_choice = previously_used[prev_used_key]
-  else:
-    for i in range(0, 20):
-      word_choice = random.choice(VARS[m])
-      if m not in previously_used or word_choice != previously_used[m]:
-        break
+    replace_pattern = re.compile(m)
+    previously_used[m] = word_choice
+    statement = replace_pattern.sub(word_choice, statement, 1)
+  return statement
 
-  replace_pattern = re.compile(m)
-  previously_used[m] = word_choice
-  statement = replace_pattern.sub(word_choice, statement, 1)
-
-
-print statement
+print process(intro)
