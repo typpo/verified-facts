ba58951ed763a58b2b3299deb9d51ed098ca3579
Ian <typppo@gmail.com>
2013-02-01 21:19:47 -0800

    add lookup page

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import json
 import random
 import base64
 import re
+from unidecode import unidecode
 
 app = Flask(__name__)
 app.secret_key = 'not a secret key'
@@ -19,11 +20,16 @@ def index():
 def verification():
   return render_template('verification.html')
 
+@app.route("/lookup")
+def verification():
+  return render_template('lookup.html')
+
 @app.route("/directory")
 def directory():
   links = []
   for x in ConspiracyGenerator().get_all_subjects():
-    val = (x[0], urllib.quote(x[0].encode('utf-8')), urllib.quote(base64.b64encode(x[1])))
+    val = (x[0], urllib.quote(x[0].encode('utf-8').replace('/', '|')),\
+        urllib.quote(base64.b64encode(x[1])))
     links.append(val)
   return render_template('directory.html', \
       links=links)
@@ -31,7 +37,7 @@ def directory():
 @app.route("/s/<subject>")
 def path(subject):
   subject_category = base64.b64decode(urllib.unquote(request.args.get('c')))
-  subject = urllib.unquote(subject)
+  subject = urllib.unquote(subject).replace('|', '/')   # ghetto escaping
   print subject_category
   print subject
   preset = {}
@@ -46,17 +52,14 @@ def generate_conspiracy_page(preset_mappings={}):
   return render_template('index.html', subject=subject, text=paragraph,\
       imgurl=imgurl, citations=citations)
 
-# TODO match slugs with category names
-"""
-from unidecode import unidecode
 _punct_re = re.compile(r'[\t !"#$%&\'()*\-/<=>?@\[\\\]^_`{|},.]+')
+
 def slugify(text, delim=u'-'):
-    #Generates an ASCII-only slug
+    """Generates an ASCII-only slug."""
     result = []
     for word in _punct_re.split(text.lower()):
         result.extend(unidecode(word).split())
     return unicode(delim.join(result))
-"""
 
 ConspiracyGenerator().verify()
 
