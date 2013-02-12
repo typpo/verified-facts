af8e84be83a2b7392f58eba9ca8261bcb8faffd6
Ian Webster <ian@room77.com>
2013-02-12 15:14:16 -0800

    Migration plan from redis to mongo

--- id_manager.py
+++ id_manager.py
@@ -3,6 +3,7 @@
 import base64
 import redis
 import json
+from pymongo import MongoClient
 
 REDIS_PREFIX = "conspiracy:pages:"
 REDIS_PAGEID_PREFIX = "conspiracy:pages:id:"
@@ -16,10 +17,12 @@ class IdManager():
     self._redis = redis.StrictRedis(host='localhost', port=6379, db=1)
     self._remote_redis = redis.StrictRedis(host=REMOTE_REDIS_HOST, port=6379, db=1)
 
-    # load in existing pages
-    pagekeys = self._redis.keys(REDIS_PAGEID_PREFIX + '*')
-    self.first_time = len(pagekeys) < 1
-    print 'Loaded %d existing keys' % (len(pagekeys))
+    self._mongo = MongoClient()
+    db = self._mongo.verified_facts
+    self._mongo_coll = db.articles
+    self._mongo_coll.ensure_index('permalink_id', unique=True)
+
+    print 'IdManager initialized with', self._mongo_coll.count(), 'articles'
 
   def get_next_id(self):
     next_id = self._redis.incr(REDIS_PREFIX + 'next_id')
@@ -30,11 +33,27 @@ class IdManager():
     self._id_to_kwargs[page_id] = args
 
     json_args = json.dumps(args)
-    # self._redis.set(REDIS_PAGEID_PREFIX + str(page_id), json_args)
-    # store in remote from now on
-    self._remote_redis.set(REDIS_PAGEID_PREFIX + str(page_id), json_args)
+
+    # Storing in mongo
+    json_args['permalink_id'] = page_id
+    self._mongo_coll.insert(json_args)
+
+    # store in remote from now on; local is out of space
+    #self._remote_redis.set(REDIS_PAGEID_PREFIX + str(page_id), json_args)
 
   def get_kwargs(self, page_id):
+    ret = self.__mongo_lookup(page_id)
+    if ret:
+      return ret
+
+    # Lookup in redis for backwards compatibility
+    return self.__redis_lookup(page_id)
+
+  def __mongo_lookup(self, page_id):
+    # TODO check type
+    return self._mongo_coll.find_one({'permalink_id': page_id})
+
+  def __redis_lookup(self, page_id):
     json_args = self._redis.get(REDIS_PAGEID_PREFIX + str(page_id))
     if not json_args:
       # try from remote
